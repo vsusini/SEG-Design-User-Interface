924079a208f31dc9b0ab13bff1f6b7a15dc41708
Vince <vmsusini@gmail.com>
2020-03-29 23:29:47 -0400

    Fixed player page Nav, added JS for SignUp

--- javascript/signInUp.js
+++ javascript/signInUp.js
@@ -1,4 +1,46 @@
 //Gathers general information about the Cheeze Wizards Game. 
 function getGeneralInfo(){
-    alert("javascript works")
+    document.getElementById("logButton").onclick = function () { logIn() };
+    document.getElementById("joinButton").onclick = function () { signUp() };
+    document.getElementById("closeButton").onclick = function () { close() };
+    document.getElementById("closeButton2").onclick = function () { close2() };
 }
+
+function logIn(){
+    var email = document.getElementById("emailAddressLog").value;
+    var pass = document.getElementById("passwordLog").value;
+
+    if (email == "" || pass == ""){
+        document.getElementById("caution").style.display = "block";
+    } else{
+        window.location.href = "userLandingPage.html"
+    }
+}
+
+function close(){
+    document.getElementById("caution").style.display = "none";
+}
+
+function close2(){
+    document.getElementById("cautionPass").style.display = "none";
+}
+
+function signUp(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("emailAddressSign").value;
+    var pass = document.getElementById("passwordSign").value;
+    var confirmPass = document.getElementById("confirmPasswordSign").value;
+
+    console.log(pass)
+    console.log(confirmPass)
+    console.log(pass == confirmPass)
+
+    if(name == "" || email == "" || pass == "" || confirmPass == ""){
+        document.getElementById("caution").style.display = "block";
+    } else if(pass != confirmPass){
+        document.getElementById("cautionPass").style.display = "block";
+    } else {
+        window.location.href = "userLandingPage.html"
+    }
+}
+
