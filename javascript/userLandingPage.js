c790858cd16c39f23fa2550d4385578dd3ab1e50
Vince <vmsusini@gmail.com>
2020-03-30 18:53:54 -0400

    Usability Update
    Stats page, changed of fontWeight when selected

--- javascript/userLandingPage.js
+++ javascript/userLandingPage.js
@@ -1,4 +1,20 @@
 //Gathers general information about the Cheeze Wizards Game. 
 function getGeneralInfo(){
-    alert("javascript works")
+    document.getElementById("points").onclick = function () { swapBoldHeader("points") };
+    document.getElementById("goals").onclick = function () { swapBoldHeader("goals") };
+    document.getElementById("assists").onclick = function () { swapBoldHeader("assists")};
+}
+
+function swapBoldHeader(id){
+    document.getElementById(id).style.fontWeight = "bolder";
+    if (id == "points"){
+        document.getElementById("goals").style.fontWeight = "normal";
+        document.getElementById("assists").style.fontWeight = "normal";
+    } else if (id == "assists"){
+        document.getElementById("goals").style.fontWeight = "normal";
+        document.getElementById("points").style.fontWeight = "normal";
+    }else{
+        document.getElementById("assists").style.fontWeight = "normal";
+        document.getElementById("points").style.fontWeight = "normal";
+    }
 }
